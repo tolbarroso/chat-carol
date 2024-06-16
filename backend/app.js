80084e863f6d2afb46afb27a6b549c97f40f910a
Carol <carolbarrosowork@gmail.com>
2024-06-16 15:20:46 -0300

    a

--- backend/app.js
+++ backend/app.js
@@ -8,22 +8,57 @@ const swaggerDocument = require('./swagger/swagger.json');
 const userRoutes = require('./routes/userRoutes');
 const externalApiService = require('./services/externalApiService');
 
+// Importar as configurações do servidor
+var app = require('./config/server');
+// const skt = require('socket.io');
+
+// Parametrizar a porta de escuta
+var server = app.listen(process.env.PORT || 5000, function () {
+    console.log('Servidor online');
+})
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+app.set('io', io);
 
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/users', userRoutes);
 
-io.on('connection', (socket) => {
-    socket.on('setUsername', (username) => {
-        socket.username = username;
-        io.emit('userJoined', username);
+// criar a conexão do websocket
+io.on('connection', function (socket) {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function () {
+        console.log('Usuário desconectou');
     });
 
-    socket.on('chatMessage', (msg) => {
-        io.emit('chatMessage', msg);
+    socket.on('msgParaServidor', function (data) {
+
+        // dialogo
+        socket.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        // participantes
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
     });
 });
 
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,36 @@
-const mongoose = require('mongoose');
+// Importar o modulo do framework express
+var express = require('express');
 
-mongoose.connect('mongodb://localhost:27017/chat-carol', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// importar um modulo do consign
+var consign = require('consign');
 
-module.exports = mongoose;
+// importar o modulo do body-parser
+var bodyParser = require('body-parser');
+
+// importar o modulo do express-validator
+// var expressValidator = require('express-validator');
+
+// iniciar o objeto do express
+var app = express();
+
+// setar as variáveis 'view engine' e 'views' do express
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+// configurar o middleware  express.static
+app.use(express.static('./app/public'));
+
+// configurar o middleware body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configurar o middleware express-validator
+// app.use(expressValidator());
+
+// efetuar o autoload das rotas, dos models e dos controllers para o objeto app
+consign()
+    .include('app/routes')
+    .then('app/controllers')
+    .into(app);
+
+// exportar o objeto app 
+module.exports = app;
